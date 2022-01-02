1ec58f7eb0badbfb6b629a80dd83fc2cf64f989a
Arpan Abhishek <arpanforbusiness@gmail.com>
2022-01-02 22:16:06 +0530

    add heroky message to the loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      Loading...
+    <div className="w-full h-screen flex justify-center text-center items-center">
+      Loading... Fetching from Heroku (for the first time) may take a few
+      seconds
     </div>
   );
 };
